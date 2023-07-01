dad156fdf28dfc77c0ce6518c8e6e7e24bb26092
Sachin Saini <sachinsaini9669.com>
2023-07-01 18:04:02 +0530

    qwd

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-DB_URL = "mongodb+srv://sachinsaini9669:wUQjMivIzEAyCuxi@todocluster.lhq3etn.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async (DATABASE_URL)=>{
     try{
