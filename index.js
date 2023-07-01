fd1dcbdbf11da33f136c433bf718adfceb1b4c68
Sachin Saini <sachinsaini9669.com>
2023-07-01 18:43:17 +0530

    wfe

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const router = require("./routes/user_routes");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/dbConnect");
 const bodyparser = require("body-parser");
-const PORT = process.env.PORT
 
 
+const PORT = process.env.PORT;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.urlencoded({extended:true}));
