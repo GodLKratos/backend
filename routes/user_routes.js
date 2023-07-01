9353b39b7adc7ce02a477bd6f0c7d1f0f852815f
Sachin Saini <sachinsaini9669.com>
2023-07-01 16:08:22 +0530

    wqdwqd

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -5,6 +5,9 @@ const UserController = require("../controllers/user_controllers")
 
 router.post("/reg",UserController.registration);
 router.post("/login",UserController.login);
+router.get("/",(req,res)=>{
+    res.send("Hello from sachin saini");
+})
 
 
 module.exports = router;
