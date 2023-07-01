2e50b2a091557c47889235d99e5837ad1aee0513
Sachin Saini <sachinsaini9669.com>
2023-07-01 18:30:02 +0530

    3e2

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const router = require("./routes/user_routes");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/dbConnect");
 const bodyparser = require("body-parser");
+const PORT = process.env.PORT || 3000
 
 
-const PORT = process.env.PORT;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/",router)
-connectDB(process.env.DB_URL);
 
-app.listen(PORT,()=>{
-    console.log('Server is running on port 2000');
+connectDB(process.env.DB_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 })
