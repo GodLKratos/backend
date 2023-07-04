03600fdc558ea42b3d7fa3bf41d661d721593bd5
Sachin Saini <sachinsaini9669.com>
2023-07-04 13:18:51 +0530

    dwd

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -39,7 +39,7 @@ class TodoController{
     static deleteData = async (req,res)=>{
         try{
             const{_id} = req.body;
-            const result = await Todo.findByIdAndDelete({_id:_id});
+            await Todo.findByIdAndDelete({_id:_id});
             res.send("Deleted");
         }
         catch(e){
